a655b349f0e0b8b6736f75a022d9f3470d0bd589
David Gowers <finticemo@gmail.com>
2018-03-30 11:57:29 +1030

    __fish_expand_userdir: fix generally, and fix suffix processing
     (ie. ~foo/ should produce '/home/foo/', and ~foo should produce '/home/foo')

--- functions/__fish_expand_userdir.fish
+++ functions/__fish_expand_userdir.fish
@@ -1,11 +1,11 @@
-
 function __fish_expand_userdir -d 'Expand user directory reference. Surely there is a better way to do this.'
   set argv (string replace -r '^~/' "$HOME/" $argv)
   set argv (string replace -r '^~$' "$HOME" $argv)
   set final
   for a in $argv
     set len (string length $a)
-    for v in (string match -rn '^~[^~();$/]+(?=/|$)' $a)
+    for v in (string match -arn '^~[^~();$/]+(?=/|$)' $a)
+      set v (string split ' ' $v)
       set start $v[1]
       set l $v[2]
       set parts
@@ -13,11 +13,11 @@ function __fish_expand_userdir -d 'Expand user directory reference. Surely there
         set -a parts (string sub -l (math start-1) $a)
       end
       # eval hackery. But reasonably safe since we disallow () $ ;
-      set -a parts (eval "echo "(string sub -s $start -l $l $a))
-      if test (math start+l) -lt len
-        set -a parts (string sub -s (math start+l))
+      eval "set -a parts "(string sub -s $start -l $l $a)
+      if test (math start+l) -le $len
+        set -a parts (string sub -s (math start+l) $a)
       end
-      set a (string join '' parts)
+      set a (string join '' $parts)
       set len (string length $a)
     end
     set -a final $a
